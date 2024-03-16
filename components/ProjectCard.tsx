a25b2ff9c2598ac15acb73959a5678430524a745
Philipp <p.bochmann@googlemail.com>
2024-03-16 09:00:27 +0100

    add image and SDGs

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,17 +1,40 @@
 import {
   Card,
-  CardHeader,
   CardBody,
   CardFooter,
   Button,
+  Box,
+  Heading,
+  AspectRatio,
+  Tag,
+  Wrap,
 } from '@chakra-ui/react'
+import NextImage from 'next/image'
 import { type Project } from '@/lib/projects/schema'
+import { getSDG } from '@/lib/projects/getSDG'
 
 const ProjectCard = ({ project }: { project: Project }) => {
   return (
     <Card>
-      <CardHeader>{project.name}</CardHeader>
-      <CardBody>{project.description}</CardBody>
+      <CardBody>
+        <AspectRatio ratio={16 / 9}>
+          <NextImage
+            src={project.image}
+            fill
+            style={{ objectFit: 'cover' }}
+            alt={project.name}
+          />
+        </AspectRatio>
+        <Heading as="h3" size="xl">
+          {project.name}
+        </Heading>
+        <Wrap>
+          {project.sdgs.map((id) => (
+            <Tag key={id}>{getSDG(id)}</Tag>
+          ))}
+        </Wrap>
+        <Box>{project.description}</Box>
+      </CardBody>
       <CardFooter>
         <Button>Add to Cart</Button>
       </CardFooter>
