42775f67d57a0763ec5fffe01c5ce2fc4a9b5651
Philipp <p.bochmann@googlemail.com>
2024-03-16 14:58:50 +0100

    add tests for add to cart logic

--- components/__tests__/AddToCart.test.tsx
+++ components/__tests__/AddToCart.test.tsx
@@ -1,10 +1,51 @@
 import AddToCart from '../AddToCart'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 describe('AddToCart', () => {
   it('should render', () => {
     render(<AddToCart id={1} max={10} price={2} />)
 
     expect(screen.getByRole('spinbutton')).toBeInTheDocument()
+  })
+
+  it('should calculate price correctly', async () => {
+    render(<AddToCart id={1} max={10} price={2} />)
+
+    expect(
+      screen.getByRole('button', {
+        name: '$20',
+      })
+    ).toBeInTheDocument()
+
+    const input = screen.getByRole('spinbutton')
+    await userEvent.clear(input)
+    await userEvent.type(input, '1')
+
+    expect(
+      screen.getByRole('button', {
+        name: '$2',
+      })
+    ).toBeInTheDocument()
+  })
+
+  it('should not allow to add more than max', async () => {
+    render(<AddToCart id={1} max={10} price={2} />)
+
+    const input = screen.getByRole('spinbutton')
+    await userEvent.type(input, '11')
+    fireEvent.blur(input)
+
+    expect(screen.getByRole('spinbutton')).toHaveAttribute('value', '10')
+  })
+
+  it('should disable button after max is added to cart', async () => {
+    render(<AddToCart id={1} max={10} price={2} />)
+
+    const input = screen.getByRole('spinbutton')
+    await userEvent.type(input, '10')
+    await userEvent.click(screen.getByRole('button'))
+
+    expect(screen.getByRole('button')).toBeDisabled()
   })
 })
